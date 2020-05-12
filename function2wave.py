78dc8606e0ed4d31b36c03556abacc57e8557d70
jotaraz <33379677+jotaraz@users.noreply.github.com>
2020-05-12 16:36:04 +0200

    Update function2wave.py

--- function2wave.py
+++ function2wave.py
@@ -6,7 +6,7 @@ f = 220              #Frequenz des Sinus-Tons in Hz
 t = 3                #Dauer des Tons in s
 p = 50               #Lautstärke von 1 bis 100   
 
-g = np.linspace(0, 1*t, samplerate*t)
-data = np.sin(2 * np.pi * f * g)
-scaled = np.int16(data/np.max(np.abs(data))*p**2.2)
-write("example.wav", samplerate, scaled)
+g = np.linspace(0, 1*t, samplerate*t) #Array mit samplerate*t Punkten von 0 bis t
+data = np.sin(2 * np.pi * f * g)      #Array enthält an i-ter Stelle Sinus(g_i)
+scaled = np.int16(data/np.max(np.abs(data))*p**2.2) #Skaliert zu einer vernünftigen Lautstärke
+write("example.wav", samplerate, scaled)            #Erstellt .wav Datei mit data Array
